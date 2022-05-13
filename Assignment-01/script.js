3d6487ad96fbb68d359d69976421ecf30c73cb07
Sara2706 <101634428+Sara2706@users.noreply.github.com>
2022-05-13 07:22:46 -0700

    Add files via upload

---
+++ Assignment-01/script.js
@@ -0,0 +1,12 @@
+let tour = "Paris";
+
+// switch case starts here
+
+switch (tour) {
+    case "U.S.A":
+        document.write("Enjoy your tour in <b>U.S.A</b> trip");
+    case "Paris":
+        document.write("Enjoy your tour in <b>Paris</b> trip");
+    default :
+        document.write("Enjoy your tour in any country");
+}
